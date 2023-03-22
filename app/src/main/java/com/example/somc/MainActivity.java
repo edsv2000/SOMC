082fde6ac459778bbad3c9390ef16add8c29ea74
Eder Salvador <edsv2000@hotmail.com>
2023-03-21 22:05:54 -0600

    try

--- app/src/main/java/com/example/somc/MainActivity.java
+++ app/src/main/java/com/example/somc/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+        getSupportActionBar().hide();
+        setContentView(R.layout.login);
     }
 }
